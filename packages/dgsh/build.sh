b90d24d4a1feacf243c57c2ea9d5546b0712e192
Further <55025025+ifurther@users.noreply.github.com>
2021-09-03 21:31:26 +0800

    change src link to git link

--- packages/dgsh/build.sh
+++ packages/dgsh/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Directed Graph Shell"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL="https://github.com/dspinellis/dgsh/archive/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=22a7f2794e1287a46b03ce38c27a1d9349d1c66535c30e065c8783626555c76c
-TERMUX_PKG_BUILD_DEPENDS="autoconf,automake,pkg-config,libtool,check,bison,git,gettext,gperf,perl,texinfo,help2man,ncurses"
-# TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/core-tools"
-# TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--host=$TERMUX_HOST_PLATFORM"
+TERMUX_PKG_SRCURL="https://github.com/dspinellis/dgsh.git"
+TERMUX_PKG_GIT_BRANCH=v${TERMUX_PKG_VERSION}
+TERMUX_PKG_DEPENDS="fftw,rsync,check,libelf"
+TERMUX_PKG_BUILD_IN_SRC=true
 
-# termux_step_pre_configure() {
-#     autoreconf -vfi
-# }
+termux_step_configure() {
+	cd $TERMUX_PKG_SRCDIR
+	sed 's/\.\/configure\ --prefix=$(PREFIX)/\.\/configure\ --prefix=$(PREFIX)\ --host=$(TERMUX_HOST_PLATFORM)/g' -i Makefile
+	sed 's/\.\/configure\ --prefix=$(PREFIX)/\.\/configure\ --prefix=$(PREFIX)\ --host=$(TERMUX_HOST_PLATFORM)/g' -i unix-tools/Makefile
+	make PREFIX=$TERMUX_PREFIX TERMUX_HOST_PLATFORM=$TERMUX_HOST_PLATFORM config
+}
