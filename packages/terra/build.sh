85030611920d448a8b41614df9b18da9c3df6cc8
Pooya Moradi <pvonmoradi@gmail.com>
2022-07-30 15:10:02 +0430

    new package: terra

---
+++ packages/terra/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/terralang/terra"
+TERMUX_PKG_DESCRIPTION="Low-level system programming language designed to \
+interoperate seamlessly with Lua"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="release/share/terra/LICENSE.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.0.4"
+TERMUX_PKG_SRCURL="https://github.com/terralang/terra/archive/refs/tags/release-1.0.4.tar.gz"
+TERMUX_PKG_SHA256=a56aaa7b8d34c0b828ab25ad055ed944132fc82adbf7afc70eb3f156be6182ea
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPENDS="clang"
