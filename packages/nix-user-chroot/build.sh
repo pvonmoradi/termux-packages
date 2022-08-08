cd1b31b188615ae2f8caf834d1a2b14e172b2f9b
Pooya Moradi <pvonmoradi@gmail.com>
2022-08-08 11:45:34 +0430

    new package: add nix-user-chroot

---
+++ packages/nix-user-chroot/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/nix-community/nix-user-chroot"
+TERMUX_PKG_DESCRIPTION="Install & run nix without root permissions"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.2.2"
+TERMUX_PKG_SRCURL="https://github.com/nix-community/nix-user-chroot/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=ef5fb2efae28ea4e9f9fbe9b4d4c021bc6edcd87f73a63624ad81c239df517e3
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
