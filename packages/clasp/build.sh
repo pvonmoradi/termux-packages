764ba366010502951e17e1716c4126639d332d2d
Pooya Moradi <pvonmoradi@gmail.com>
2022-09-18 16:06:30 +0430

    new package: clasp

---
+++ packages/clasp/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/clasp-developers/clasp"
+TERMUX_PKG_DESCRIPTION="A Common Lisp implementation based on LLVM with C++ integration"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE_FILE="licenses/LGPL"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.0.0"
+TERMUX_PKG_SRCURL="https://github.com/clasp-developers/clasp/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BUILD_DEPENDS=""
+TERMUX_PKG_DEPENDS=""
+
+termux_step_configure() {
+}
