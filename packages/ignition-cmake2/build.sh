d1bfee4e1100523251c7038768a8504f2a4fccb2
Pooya Moradi <pvonmoradi@gmail.com>
2022-10-03 10:30:35 +0330

    new package: ignition-cmake2

---
+++ packages/ignition-cmake2/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/gazebo-release/gz_cmake2_vendor"
+TERMUX_PKG_DESCRIPTION="Vendor package for providing ign-cmake2 in ROS2"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Pooya Moradi <pvonmoradi@gmail.com>"
+TERMUX_PKG_VERSION="0.0.2"
+TERMUX_PKG_SRCURL="https://github.com/gazebo-release/gz_cmake2_vendor/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=5b13472e33db9c9bb631b037d1c6b0ccedfcf5e38e16157024cc641215a43b32
+TERMUX_PKG_AUTO_UPDATE=true
+# FIXME ERROR ament_cmake_test
+# TERMUX_PKG_DEPENDS="libc++"
+# TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+# "
